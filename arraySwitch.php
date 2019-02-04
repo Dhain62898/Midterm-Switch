78cf3d7546be73703fc656edbd65721b6d74ef1e
Dhain62898 <danielledominic00@gmail.com>
2019-02-04 14:56:47 +0800

    2nd commit

---
+++ arraySwitch.php
@@ -0,0 +1,17 @@
+ <?php
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "black":
+        echo "Your favorite color is black!";
+        break;
+    case "blue":
+        echo "Your favorite color is pink!";
+        break;
+    case "green":
+        echo "Your favorite color is orange!";
+        break;
+    default:
+        echo "Your favorite color is neither black, pink, nor orange!";
+}
+?> 
